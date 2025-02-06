6e782d848f831d46bb91f6b464b280b77916e24d
Diegoandres-bit <diegoandresosorio8@gmail.com>
2025-02-05 22:23:36 -0500

    se corrigieron los primero 3 puntos del ejercicio 3 para que se anadiera Stream dentro de la solucion del problema y tambien se implemento el ultimo punto del ejercicio 3

--- src/main/java/com/mycompany/bases/apps/empresariales/Ejercicio3.java
+++ src/main/java/com/mycompany/bases/apps/empresariales/Ejercicio3.java
@@ -8,47 +8,53 @@ import java.util.Arrays;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author DIEGO ANDRES
  */
 public class Ejercicio3 {
+
     public static void main(String[] args) {
-         ArrayList<String> nombres = new ArrayList<>(Arrays.asList( "gato", "perro", "flor", "piedra", "sol", "Luna", "estrella", "montaña", "Agua","Arroz","azucar"
+        ArrayList<String> nombres = new ArrayList<>(Arrays.asList("gato", "gato", "gato", "perro", "flor", "piedra", "sol", "Luna", "estrella", "montaña", "agua", "Arroz", "azucar", "Gato", "Perro", "Manzana", "Sol", "Luna", "Estrella", "Cielo", "sol", "Montaña", "Río", "Mar", "Noche", "perro", "flor", "piedra", "perro", "flor", "piedra", "perro", "flor", "piedra", "Árbol", "Viento", "Flor", "Viento", "Agua", "Piedra", "Lluvia", "Fuego", "Flor", "Viento", "Agua", "Vaca", "León", "Tigre", "Delfín", "Gato", "Coche", "Camión", "Avión", "Tren", "Lámpara", "Silla", "Mesa", "Libro", "Pelota", "Zapato", "Pantalón", "Camisa", "Falda", "Sombrero", "Ratón", "Conejo", "Águila", "Plátano", "Kiwi", "Naranja", "Fresa", "Cereza", "Pera", "Mango", "Papaya", "Uva", "Carro", "Bicicleta", "Mochila", "Espejo", "Reloj", "Teclado", "Computadora", "Teléfono", "Casa", "Ventana", "Puerta", "Almohada", "Sofá", "Refrigerador", "Cocina", "Cortina", "Escalera", "Fruta", "Dulce", "Pastel", "Tarta", "Pan", "Leche", "Café", "Té", "Chocolate", "Pizza", "Pasta", "Empanada", "Arroz", "Sopa", "Tortilla", "Ensalada", "Queso", "Pollo", "Carne", "Pescado", "Verdura", "Espinaca", "Zanahoria", "Tomate", "Cebolla", "Ajo", "Pimiento", "Pepino", "Lechuga", "Maíz", "Frijoles", "Garbanzo", "Arroz", "Azúcar", "Sal", "Pimienta", "Aceite", "Vinagre", "Salsa", "Mostaza", "Mantequilla", "Yogur", "Helado", "Miel", "Frambuesa", "Mora", "Sandía", "Melón", "Limón", "Nuez", "Almendra", "Pecan", "Cacahuate"
         ));
-         System.out.println("Filtrar los nombres que comiencen con 'A'.");
-          for (int i = 0; i < nombres.size(); i++) {
-              
-                char [] letra =nombres.get(i).toCharArray();
-              if('A'==(letra[0])){
+        System.out.println("Filtrar los nombres que comiencen con 'A'.");
+        
+
+            nombres.stream()
+               .filter(nombre -> nombre.charAt(0) == 'A') 
+               .forEach(System.out::println);
+
+        
+        System.out.println("");
+        System.out.println("Convertir todos los nombres a mayusculas.");
+        System.out.println("");
+          nombres.stream()
+               .map(String::toUpperCase)
                   
-                  System.out.println(nombres.get(i));
-              }
+               .forEach(System.out::println); 
+          System.out.println("Contar cuantos nombres tienen mas de 5 letras.");
+        long contador = nombres.stream()
+                               .filter(nombre -> nombre.length() > 5) 
+                               .count();
+        
+        
+        System.out.println("Hay " + contador + " palabras con mas de 5 letras.");
+
+        System.out.println("");
+              
+             nombres.stream()
+               .distinct() 
+               .forEach(palabra -> {
+                   long repeticiones = nombres.stream()
+                                               .filter(p -> p.equals(palabra)) 
+                                               .count(); 
+                   System.out.println( palabra + "' se repite " + repeticiones + " veces.");
+               });
+         
            
-        }
-          System.out.println("");
-          System.out.println("Convertir todos los nombres a mayusculas.");
-          System.out.println("");
-           for (int i = 0; i < nombres.size(); i++) {
-                
-                System.out.println(nombres.get(i).toUpperCase());
-            }
-           int contador=0;
-            System.out.println("Contar cuantos nombres tienen mas de 5 letras.");
-            for (int i = 0; i < nombres.size(); i++) {
-               
-               
-                if(nombres.get(i).length()>5){
-                    contador++;
-                }
-                
-            }
-            System.out.println("Hay"+contador+"palabras con 5 letras");
-            
-            
-          
+
+        
+
     }
-   
-    
+
 }
